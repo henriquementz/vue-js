a108588b263e5a04314370e07a41f24e30129110
Henrique Mentz <henrique-mentz@hotmail.com>
2017-10-19 20:04:37 -0200

    Adicionar component de mural para fotos.

--- Vue/mural-de-fotos/src/App.vue
+++ Vue/mural-de-fotos/src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
-      </li>
-    </ul>
-
-  </div>
+   <div class="corpo">
+      <h1 class="centralizado">{{ titulo }}</h1>
+      <input type="search" class="filtro" v-on:input="filtro=$event.target.value" placeholder="filtre pelo título da foto">
+      <ul class="lista-fotos">
+         <li class="lista-fotos-item" v-for="foto in fotos">
+            <meu-painel :titulo="foto.titulo">
+               <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+            </meu-painel>
+         </li>
+      </ul>
+   </div>
 </template>
 
+
 <script>
+import Painel from './components/shared/painel/Painel.vue'
+
 export default {
+
+  components: {
+    'meu-painel': Painel
+  },
 
   data() {
 
     return {
 
-      titulo: 'Alurapic', 
-      fotos: []
+      titulo: 'Mural de Fotos', 
+      fotos: [],
+      filtro: ''
     }
   },
 
   created() {
-
     this.$http.get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
@@ -34,4 +42,54 @@ export default {
 </script>
 
 <style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 aut
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  /* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px gre
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  .filtro {
+    display: bloc
+    width: 100%;
+  }
 </style>
