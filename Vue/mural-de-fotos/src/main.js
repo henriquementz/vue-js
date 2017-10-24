e7aabd782a445d5e5f61e33d1eed220d5e924d7e
Henrique Mentz <henrique-mentz@hotmail.com>
2017-10-24 21:43:39 -0200

    Adicionar config vue-router e iniciar configuracao da Single Page Application.

--- Vue/mural-de-fotos/src/main.js
+++ Vue/mural-de-fotos/src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
 
+import VueRouter from 'vue-router';
+
+import { routes } from './routes';
+
+const router = new VueRouter({ routes, mode: 'history' });
+
+//Es6 - Parâmetro e valor com menos nome. 
+
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
