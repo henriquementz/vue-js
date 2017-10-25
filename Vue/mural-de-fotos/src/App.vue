1766fd0bf932376432db89cd29284c1679e714c6
Henrique Mentz <henrique-mentz@hotmail.com>
2017-10-25 21:15:37 -0200

    Adicionar menu.

--- Vue/mural-de-fotos/src/App.vue
+++ Vue/mural-de-fotos/src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="corpo">
-   <router-view></router-view>
+
+    <nav>
+      <ul>       
+          <li v-for="route in routes">
+            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+
   </div>
 </template>
-
 <script>
+
+import { routes }  from './routes';
+
 export default {
+
+  data() {
+
+    return {
+
+      routes
+    }
+
+  }
+
 }
 </script>
 <style>
@@ -14,5 +38,12 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
   }
 </style>
--- Vue/mural-de-fotos/src/routes.js
+++ Vue/mural-de-fotos/src/routes.js
@@ -1,7 +1,9 @@
-import Cadastro from './components/cadastro/Cadastro.vue';
 import Home from './components/home/Home.vue';
+import Cadastro from './components/cadastro/Cadastro.vue';
 
 export const routes = [
-    {path: '', component: Home},
-    {path: '/cadastro', component: Cadastro}
+
+    { path: '', component: Home, titulo: 'Home' },
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
+
 ];
