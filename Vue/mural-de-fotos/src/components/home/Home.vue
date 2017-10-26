91ad14edb7b1a4ccdb96572ad30f1303dcc137ae
Henrique Mentz <henrique-mentz@hotmail.com>
2017-10-26 21:55:00 -0200

    Adicionarestrutura inicial para exclusão das fotos.

--- Vue/mural-de-fotos/src/components/home/Home.vue
+++ Vue/mural-de-fotos/src/components/home/Home.vue
@@ -9,6 +9,8 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove($event, foto)" /> 
+          
         </meu-painel>
       </li>
     </ul>
@@ -19,14 +21,17 @@
 <script>
 //@ - view para fonte de dados.
 //: fonte de dados para a view.
+// sem ':' : não tem associação com dado do componente
 import Painel from '../shared/painel/Painel.vue'
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue'
 
 export default {
 
   components: {
     'meu-painel': Painel, 
-    'imagem-responsiva': ImagemResponsiva
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
   },
 
   data () {
@@ -59,7 +64,13 @@ export default {
       .get('http://localhost:3000/v1/fotos')
       .then(res => res.json())
       .then(fotos => this.fotos = fotos, err => console.log(err));
-  }
+  }, 
+
+   methods:{
+      remove($event, foto){
+        alert('Excluido com sucesso ' + foto.titulo + $event)
+      }
+    }
 }
 </script>
 <style>
--- Vue/mural-de-fotos/src/components/shared/painel/Painel.vue
+++ Vue/mural-de-fotos/src/components/shared/painel/Painel.vue
@@ -23,7 +23,9 @@ export default {
       return {
         visivel: true
       }
-    }
+    },
+
+   
 }
 </script>
 <style scoped>
